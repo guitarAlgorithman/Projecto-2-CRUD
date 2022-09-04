9e5f35319b49e14f38f2958b8cbb7f2dcb857179
Gustavo Henriquez <gustavo.henriquez.m@gmail.com>
2022-09-04 09:08:13 -0300

    falta el update

--- static/script.js
+++ static/script.js
@@ -8,6 +8,14 @@ create = () => {
         'phone': form['phone'].value,
         'genero': form['inputState'].value,
     }
+    //Simple check de todo vacio
+    if(entrada.nombre=="" || entrada.email=="" || entrada.phone==""){
+        alert("Ingrese nombre, email y telefono ");
+        reset();
+        location.reload()    
+        return 0;
+    }
+    
     //localStorage.setItem(String(localStorage.length+1),JSON.stringify(entrada));
     guardarData(entrada)
     reset();
@@ -31,7 +39,10 @@ guardarData = (entrada) => {
 }
 //Limpia el formulario
 reset = () => {
+
     document.getElementById('formulario').reset();
+    const boton = document.getElementsByClassName("btn1")
+    boton.innerHTML="Create";
 }
 
 // //Carga
@@ -44,24 +55,50 @@ showAll = () => {
         let datosParseados = JSON.parse(guardados);
         console.log(datosParseados);
         for (let i = 0; i < datosParseados.length; i++) {
-            document.getElementById("aca").innerHTML += "<tr id=" + i + "><th>" + String(i + 1) + "</th><td>" + datosParseados[i].nombre + "</td><td>" + datosParseados[i].email + "</td><td>" + datosParseados[i].phone +  "</td><td>" + datosParseados[i].genero + "</td><td><button class="+"'"+"buttonTable"+"'"+" id=" + i + " onclick=" + "'" + "borra(this.id)" + "'" + ">Delete</button><button class="+"'"+"buttonTable"+"'"+">"+"Actualizar</button></td></tr>"
+            document.getElementById("aca").innerHTML += "<tr id=" + i + "><th>" + String(i + 1) +
+             "</th><td>" + datosParseados[i].nombre + "</td><td>" + datosParseados[i].email +
+              "</td><td>" + datosParseados[i].phone + "</td><td>" + datosParseados[i].genero +
+              "</td><td><button class=" + "'" + "buttonTable" + "'" + " id=" + i + " onclick=" +
+               "'" + "borra(this.id)" + "'" + ">Delete</button><button "+"id=" + "'" + i + "'" +" onclick=" + "'" + "update(this.id)" + "'" + ">" +
+                "Actualizar</button></td></tr>"
         }
     }
 }
 
-function borra(id){
+function borra(id) {
     console.log(id);
     //Obtengo los datos desde el storage
     let guardados = localStorage.getItem("data");
-    if(guardados){
+    if (guardados) {
         let datosParseado = JSON.parse(guardados);
         arr = datosParseado.filter(item => item !== datosParseado[parseInt(id)])
-        console.log(datosParseado)
-        localStorage.setItem("data", JSON.stringify(arr));
+        localStorage.setItem("data", JSON.stringify(arr));        
     }
     location.reload()
 
-}   
+}
+
+
+update=(id)=>{
+    //Obtengo los datos desde el storage
+    let guardados = localStorage.getItem("data");
+    if (guardados) {
+        let datosParseado = JSON.parse(guardados);
+        //busco el elemento con el id
+        const form = document.getElementById('formulario');        
+        form['nombre'].value=datosParseado[id].nombre;
+        form['email'].value=datosParseado[id].email;
+        form['phone'].value=datosParseado[id].phone;
+        form['inputState'].value=datosParseado[id].genero; 
+        const boton = document.getElementById("botonCambia")
+        boton.innerHTML="Update";
+        boton.setAttribute("posicion",id)
+
+    }
+  
+}
+
+cargaActulizado(id)
 
 showAll()
 
